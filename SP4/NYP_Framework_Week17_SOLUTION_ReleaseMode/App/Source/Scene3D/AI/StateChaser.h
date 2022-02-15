93ebe7e80cf1fd80abf5579777aa95202aea6a50
HGWH00 <54061467+HGWH00@users.noreply.github.com>
2022-02-15 09:55:46 +0800

    Update StateChaser.h

--- SP4/NYP_Framework_Week17_SOLUTION_ReleaseMode/App/Source/Scene3D/AI/StateChaser.h
+++ SP4/NYP_Framework_Week17_SOLUTION_ReleaseMode/App/Source/Scene3D/AI/StateChaser.h
@@ -4,24 +4,24 @@
 #include "State.h"
 #include "GameObject.h"
 
-class StateIdle2 : public State
+class StateIdle : public State
 {
 	GameObject* m_go;
 public:
-	StateIdle2(const std::string& stateID, GameObject* go);
-	virtual ~StateIdle2();
+	StateIdle(const std::string& stateID, GameObject* go);
+	virtual ~StateIdle();
 
 	virtual void Enter();
 	virtual void Update(double dt);
 	virtual void Exit();
 };
 
-class StateChase2 : public State
+class StateChase : public State
 {
 	GameObject* m_go;
 public:
-	StateChase2(const std::string& stateID, GameObject* go);
-	virtual ~StateChase2();
+	StateChase(const std::string& stateID, GameObject* go);
+	virtual ~StateChase();
 
 	virtual void Enter();
 	virtual void Update(double dt);
